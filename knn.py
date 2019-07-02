c45c3a2b75a743e0ea793e97eb14be7d2c8145f6
kamaboco <okod129@gmail.com>
2019-07-02 18:56:10 +0900

    完成

---
+++ knn.py
@@ -0,0 +1,59 @@
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
+
+class kNN:
+    _dists = []
+    def __init__(self, k, train_img, train_l):
+        self._train_img = train_img
+        self._train_l = train_l
+        self._k = k
+    
+    def _dist(self, p, q):
+        # Euclidean distance
+        return np.linalg.norm(p-q, ord=2)
+
+    def predict(self, x, i):
+        # make dists table
+        if len(self._dists) < i+1:
+            self._dists.append(np.array([self._dist(np.ravel(j), np.ravel(x)) for j in self._train_img]))
+
+        # sort
+        nearest_indexes = self._dists[i].argsort()[:self._k]
+        nearest_labels = self._train_l[nearest_indexes]
+        
+        c = Counter(nearest_labels)
+        # return most common label
+        return c.most_common(1)[0][0]
+
+
+def main():
+    # load data
+    (train_img, train_l), (test_img, test_l) = mnist.load_data()
+
+    # show data num, label num
+    print("train img num  :", train_img.shape)
+    print("train label num:", train_l.shape)
+    print("test img num   :", test_img.shape)
+    print("test label num :", test_l.shape)
+
+    # learn process
+    for k in range(1, test_img.shape[0]):
+        match_count = 0
+        # create model
+        model = kNN(k, train_img, train_l)
+        print("{} NN start".format(k))
+        for i in range(0, test_img.shape[0]):
+            label = model.predict(test_img[i], i)
+            if test_l[i] == label:
+                match_count += match_count + 1
+            if i % (test_img.shape[0]/100) == 0:
+                print("")
+            print("*", end="")
+
+        # show img complete parsent    
+        print("error rate:", 1 - match_count / test_img.shape[0])
+
+if __name__ == '__main__':
+    main()
